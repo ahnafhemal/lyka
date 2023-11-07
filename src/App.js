fc8f9819c77523708671bc00d95c62b24d2d9b73
ahnafhemal <hemalhasan140@gmail.com>
2023-11-07 20:51:57 +0600

    second

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+
+import './App.css'
+import { ChakraProvider } from '@chakra-ui/react'
+import Header from './Components/Header/Header'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='offer-section'>
+        <p> <a href="/#">Get started With 20% off your first box,<span>click to redeem</span></a> </p>
+      </div>
+
+      <ChakraProvider>
+        <Header></Header>
+      </ChakraProvider>
+
+
+
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
---
+++ src/Components/Header/Header.js
@@ -0,0 +1,16 @@
+import { Heading, Link, Text } from '@chakra-ui/react'
+import React from 'react'
+
+
+const Header = () => {
+    return (
+        <div>
+            <Heading>Lyka</Heading>
+            <h1>HI</h1>
+
+        </div>
+    )
+}
+
+export default Header
+
